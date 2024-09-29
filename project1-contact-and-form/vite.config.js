3dc55283ee4812cea480e640b7d67e9987494d36
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2024-09-29 22:38:54 +0545

    made responsive

--- project1-contact-and-form/vite.config.js
+++ project1-contact-and-form/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This will allow access from your network devices
+    port: 5174, // Default Vite port, change if needed
+  },
 })
